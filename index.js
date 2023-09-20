93e645670968f4ba5cc030b0260cf10272b4e88e
Wellington <125682138+jesussalvaeliberta2023@users.noreply.github.com>
2023-09-20 13:29:01 -0300

    Certinho

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Wellington de Oliveira do Nascimento - 20/09/2023');
+  var atual = new Date();
+  var publicada = atual.toLocaleDateString('pt-BR');
+    
+  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+  res.end('<h1 style="font-size: 20px;">Wellington de Oliveira do Nascimento </h1>\n<p>Data atual: ' + publicada + '</p>');
 }).listen(8008);
